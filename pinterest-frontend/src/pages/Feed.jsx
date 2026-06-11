997f7dacb2c255b48f35fae8f1830e56252d7049
PepePerezzz <alonsoherrera847@gmail.com>
2026-06-11 01:43:05 -0600

    refactorizacion de estilos y documentacion de codigos

--- pinterest-frontend/src/pages/Feed.jsx
+++ pinterest-frontend/src/pages/Feed.jsx
@@ -1,85 +1,38 @@
-/*import PostCard from "../components/PostCard";
-import Navbar from "../components/Navbar";
-import "../styles/Feed.css";
-
-function Feed() {
-  //Post fake para probar 
-  // 😂
-const posts = [
-  {
-    id: 1,
-    author: "Leonardo",
-    text: "La disciplina supera a la motivación. Si quieres lograr algo, hazlo todos los días, incluso cuando no tengas ganas. "
-  },
-  {
-    id: 2,
-    author: "Ana",
-    text: "Hoy aprendí React."
-  },
-  {
-    id: 3,
-    author: "Carlos",
-    text: "Pensamiento corto."
-  }
-];
-
-  return (
-    <>
-      <Navbar></Navbar>
-      <div className="feed-container">
-        <div className="masonry">
-          {posts.map((post) => (
-            <PostCard
-              key={post.id}
-              author={post.author}
-              text={post.text}
-            />
-          ))}
-        </div>
-      </div>
-    </>
-  );
-}
-
-export default Feed;*/
-
-import React, { useState, useEffect } from "react"; // 🌟 Revisa que tenga 'useEffect' adentro
-import axios from "axios";                         // 🌟 ¡Esta línea es obligatoria para las peticiones!
+import React, { useState, useEffect } from "react"; 
+import axios from "axios";                         
 import PostCard from "../components/PostCard";
 import Navbar from "../components/Navbar";
 import CreatePinModal from "../components/CreatePinModal";
 import "../styles/Feed.css";
 
 function Feed() {
-// 1. Control del Modal (Ya lo tenías)
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  // 2. Estado para guardar los posts que vengan de la base de datos (Empieza vacío)
   const [posts, setPosts] = useState([]);
-
-  // 3. Estado de carga obligatorio por rúbrica (Empieza en true)
   const [loading, setLoading] = useState(true);
 
-  // 4. useEffect para pedir los textos al Backend en cuanto cargue la página
+  //useEffect para pedir los textos al Backend en cuanto cargue la pagina
   useEffect(() => {
-    // Aquí usamos Axios con una Promesa (.then / .catch) como pide el PDF
-    axios.get('http://localhost:3000/api/pins')
-      .then((respuesta) => {
-        // Guardamos los registros de la base de datos en nuestro estado
-        setPosts(respuesta.data); 
-        setLoading(false); // Apagamos el mensaje de "Cargando..."
-      })
-      .catch((error) => {
-        console.error("Error al conectar con el servidor:", error);
-        setLoading(false); // Apagamos el cargando aunque falle
-      });
-  }, []); // Los corchetes vacíos aseguran que solo se ejecute al montar el componente
+    //Axios con una Promesa (.then / .catch)
+    axios.get('http://localhost:3000/api/pins') //se crea la Promesa
+    .then((respuesta) => {
+    //Si la promesa se CUMPLE (Fulfilled), entramos aqui
+    //Recibimos los los datos de MySQL y los guardamos
+    setPosts(respuesta.data); 
+    setLoading(false);
+  })
+  .catch((error) => {
+    //si la promesa se RECHAZA, brinca directo aqui
+    // Atrapamos el error y avisamos al usuario.
+    console.error(error);
+    setLoading(false);
+  });
+  }, []); 
 
-  // 5. Renderizado condicional de carga: Si está cargando, muestra un mensaje
+  //Renderizado condicional de carga: Si está cargando, muestra un mensaje
   if (loading) {
     return (
       <div style={{ padding: '20px', textAlign: 'center' }}>
-        <h2>Cargando pensamientos desde la base de datos... ⏳</h2>
+        <h2>Cargando pensamientos desde la base de datos... </h2>
       </div>
     );
   }
@@ -93,8 +46,6 @@ const handleCreatePost = async (newPostData) => {
         texto: newPostData.text
       };
 
-
-      // Pregúntale al del Backend si su endpoint es '/api/pins' o '/api/pines'
       const respuesta = await axios.post('http://localhost:3000/api/pins', datosParaBD);
       
       const postGuardado = respuesta.data;
@@ -104,7 +55,7 @@ const handleCreatePost = async (newPostData) => {
 
     } catch (error) {
       console.error("Error al guardar en la base de datos:", error);
-      alert("❌ Error: No se pudo conectar con el servidor o falló el guardado.");
+      alert("Error: No se pudo conectar con el servidor o falló el guardado.");
     }
   };
 
@@ -145,7 +96,7 @@ return (
   );
 }
 
-// 🖌️ Estilo del botón flotante redondo (Fijo en la esquina inferior derecha)
+
 const floatingButtonStyle = {
   position: "fixed",
   bottom: "40px",
@@ -164,7 +115,7 @@ const floatingButtonStyle = {
   justifyContent: "center",
   alignItems: "center",
   transition: "transform 0.2s ease",
-  zIndex: 999, // Un nivel abajo del fondo difuminado pero arriba de las tarjetas
+  zIndex: 999, 
 };
 
 export default Feed;
