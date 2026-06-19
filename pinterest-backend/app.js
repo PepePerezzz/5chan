93bdc719977cbb7f85040ae74895a0e9c04f2f28
Edson Leonardo <edsonleonardosanchezmontalvo@gmail.com>
2026-06-19 14:46:26 -0600

    fix(fullstack):separe las partes del proyecto para subirlo a produccion

--- pinterest-backend/app.js
+++ pinterest-backend/app.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const authRoutes = require("./routes/authRoutes");
 const pinRoutes = require("./routes/pinRoutes");
 const userRoutes = require("./routes/userRoutes");
+
+const PORT = process.env.PORT || 3000;
+
 
 
 app.use(cors());
@@ -19,6 +21,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/pins", pinRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Servidor en puerto ${process.env.PORT}`);
+app.listen(PORT, () => {
+    console.log(`Servidor en puerto ${PORT}`);
 });
